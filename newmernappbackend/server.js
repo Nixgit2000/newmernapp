ecb6836e84c67e14fc7feaa21149603b106058d6
Nixgit2000 <fourwaycoordination@gmail.com>
2023-02-18 15:42:52 -0500

    commit

--- newmernappbackend/server.js
+++ newmernappbackend/server.js
@@ -2,7 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import movies from './api/movies.route.js'
 
-export default app = express();
+var app = express();
 app.use(cors())
 app.use(express.json())
 
@@ -10,3 +10,5 @@ app.use("/api/v1/movies", movies)
 app.use("*",(req,res) => {
     res.status(404).json({error: "not found"})
 })
+
+export default app
